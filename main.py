8fe56ab4183203acc6c8a312e420bac9428823c6
Zack Connaughton <zaconnaughton@gmail.com>
2019-04-06 22:59:33 -0700

    Added an add function to the main.py to render the create_donation page in templates. Updated base.jinja to include navigation links at the top of the page

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import base64
 
 from flask import Flask, render_template, request, redirect, url_for, session
 
-from model import Donation 
+from model import Donation
 
 app = Flask(__name__)
 
@@ -15,9 +15,12 @@ def home():
 def all():
     donations = Donation.select()
     return render_template('donations.jinja2', donations=donations)
-    
+
+
+@app.route('/add/')
+def add():
+    return render_template('create_donation.jinja2')
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6738))
     app.run(host='0.0.0.0', port=port)
-
